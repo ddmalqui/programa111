eb6ab3609a87547305794a00fbaac58a19ec5e3d
ddmalqui <ddmalqui@gmail.com>
2017-11-02 16:57:59 -0300

    Practica

---
+++ Practica/holaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Diego
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic
+        		System.out.println("Hello World!");
+    }
+    
+}
