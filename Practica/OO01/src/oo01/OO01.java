91a3076242803fe075933b250e7b680f0d8dbd1a
Diego <ddmalqui@gmail.com>
2017-11-21 08:15:20 -0300

    Update OO01.java
    Tareas

--- Practica/OO01/src/oo01/OO01.java
+++ Practica/OO01/src/oo01/OO01.java
@@ -19,6 +19,20 @@ public class OO01 {
         
         System.out.print("cantidad de Lados: " + circulo.cantLados + "\n");
         
+         //Imprimir el resto de las propiedades del objeto Circulo.
+        
+        //Crear una clase Cuadrado, con 6 propiedades
+        //Instanciar la clase
+        //Imprimir sus propiedades
+        
+        //Crear una clase Triangulo, con 6 propiedades
+        //Instancia la clase. 
+        //La clase triangulo, debe poder construirse pasandole los 6 paramentros. O sea, el contructor de la clase debe poder recibir 6 paramentro.
+        
+        //Crear los setter y getter en Circulo. (comentar donde se imprimen las propiedades directamente).
+        //Crear otra instancia de circulo.
+        //Crear un metodo Circunferencia en Circulo, que retorne la Circunferencia del Objeto circulo (dos veces el radio (r) por π).
+        //setiar el radio de los circulos en 10 y 20 (Ambos radios en cada circulo).
         
     }
     
